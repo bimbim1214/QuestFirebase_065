486c9846231955e36da2243e46f9f272937d2dd6
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-12 15:41:43 +0700

    Tambahkan komponen Composable InsertMhsView untuk menampilkan halaman tambah mahasiswa dengan validasi form, snackbar, dan navigasi.

--- app/src/main/java/com/example/myapplication/ui/view/InsertView.kt
+++ app/src/main/java/com/example/myapplication/ui/view/InsertView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,10 +12,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,7 +32,84 @@ import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.viewmodel.FormErrorState
 import com.example.myapplication.ui.viewmodel.FormState
 import com.example.myapplication.ui.viewmodel.InsertUiState
+import com.example.myapplication.ui.viewmodel.InsertViewModel
 import com.example.myapplication.ui.viewmodel.MahasiswaEvent
+import com.example.myapplication.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = androidx.lifecycle.viewmodel.compose.viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.uiState // State utama uuntuk loading, Success, Error
+    val uiEvent = viewModel.uiEvent // State untuk form dan validasi
+    val snackbarHostState = remember { SnackbarHostState()}
+    val coroutineScope = rememberCoroutineScope()
+
+    // Observasi perubahan state untuk snackbar dan navigasi
+    LaunchedEffect(uiState) {
+        when (uiState) {
+            is FormState.Success -> {
+                println(
+                    "InsertMhsView: uiState is FormState.Success, navigate to home" + uiState.message
+                )
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)//tampilkan snakcbar
+                }
+                delay(700)
+                onNavigate()//navigasi langsung
+
+                viewModel.resetSnackBarMessage()//reset snackbar state
+            }
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState)},
+        topBar = {
+            TopAppBar(
+                title = { Text("Tambah Mahasiswa")},
+                navigationIcon = {
+                    Button(onClick = onBack) {
+                        Text("Back")
+                    }
+                }
+            )
+        }
+    ) {padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            InsertBodyMhs(
+                uiState = uiEvent,
+                homeUiState =uiState,
+                onValueChange ={updateEvent ->
+                               viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    if (viewModel.validateFieds()){
+                        viewModel.insertMahasiswa()
+                    }
+                }
+            )
+        }
+
+    }
+}
 
 
 @Composable
