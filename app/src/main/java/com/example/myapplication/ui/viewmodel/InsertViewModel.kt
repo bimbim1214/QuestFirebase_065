4f092622afbdc92362e19a22ffda92afaba2f2bd
bimbim1214 <bimoadityapangestu@gmail.com>
2025-01-12 11:23:58 +0700

    Tambahkan data class MahasiswaEvent untuk merepresentasikan event data input mahasiswa.

--- app/src/main/java/com/example/myapplication/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/InsertViewModel.kt
@@ -3,6 +3,14 @@ package com.example.myapplication.ui.viewmodel
 import com.example.myapplication.model.Mahasiswa
 
 
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
 
 fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa(
     nim = nim,
